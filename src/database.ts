bfcb5b70aabd291042a50dedcd4764e0b265038d
Vitor Andrey <vitor.andreylopess@gmail.com>
2023-10-28 17:10:08 -0300

    feat: add zod validation and .env

--- src/database.ts
+++ src/database.ts
@@ -1,10 +1,11 @@
 import "dotenv/config"
 import { knex as setupKnex, Knex } from "knex";
+import { env } from "./env";
 
 export const knexConf: Knex.Config = {
   client: "sqlite",
   connection: {
-    filename: process.env.DATABASE_URL || "",
+    filename: env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
